18c7375be553f020d1bb5b16880e0439a5e015c1
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-07-05 13:02:54 +0700

    test _captureImage

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -3,66 +3,42 @@ import {
   View,
   StyleSheet,
   Dimensions,
-  Pressable,
+  TouchableOpacity,
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
-import ImgToBase64 from 'react-native-image-base64';
 import { TfImageRecognition } from 'react-native-tensorflow';
+import { RNCamera } from 'react-native-camera';
+
+const tfImageRecognition = new TfImageRecognition({
+  model: require('../assets/tensorflow_inception_graph.pb'),
+  labels: require('../assets/tensorflow_labels.txt'),
+  imageMean: 117, // Optional, defaults to 117
+  imageStd: 1 // Optional, defaults to 1
+})
 
 export default function Camera(props) {
   const cameraRef = useRef();
   const [isProcessing, setIsProcessing] = useState(false);
 
-  const handleImageCapture = async () => {
+  async function _captureImage() {
     try {
-      setIsProcessing(true);
       const image = await cameraRef.current.takePictureAsync().catch((error) => {
         throw error;
       });
-      ImgToBase64.getBase64String(image.uri)
-        .then(base64String => {
-          processImagePrediction(base64String);
-        })
-        .catch(error => {
-          console.log('base64String => ', error);
-        });
-    } catch (error) {
-      console.log('handleImageCapture => ', error);
-    }
-  };
-
-  const processImagePrediction = async (base64Image) => {};
-
-  useEffect(() => {
-    async function _loadData() {
-      
-
-      const tfImageRecognition = new TfImageRecognition({
-        model: require('../assets/tensorflow_inception_graph.pb'),
-        labels: require('../assets/tensorflow_labels.txt'),
-        imageMean: 117, // Optional, defaults to 117
-        imageStd: 1 // Optional, defaults to 1
-      })
-
       const results = await tfImageRecognition.recognize({
-        image: require('../assets/dumbbell.jpg'),
+        image: image.uri,
         inputName: "input", //Optional, defaults to "input"
         inputSize: 224, //Optional, defaults to 224
         outputName: "output", //Optional, defaults to "output"
         maxResults: 3, //Optional, defaults to 3
         threshold: 0.1, //Optional, defaults to 0.1
       })
-
-      results.forEach(result =>
-        console.log(
-          result.id, // Id of the result
-          result.name, // Name of the result
-          result.confidence // Confidence value between 0 - 1
-        )
-      )
+      console.log('results => ', results);
+    } catch (error) {
+      console.log('error => ', error);
+    } finally {
+      // _captureImage();
     }
-    _loadData()
-  }, [])
+  }
 
   return (
     <View style={styles.container}>
@@ -71,14 +47,16 @@ export default function Camera(props) {
         ratio={'16:9'}
         focusDepth={0}
         ref={cameraRef}
+        camera1ScanMode={'boost'}
         captureAudio={false}
-        style={styles.container}
+        style={styles.camera}
         type={RNCamera.Constants.Type.front}
         autoFocus={RNCamera.Constants.AutoFocus.on}
-        whiteBalance={RNCamera.Constants.WhiteBalance.auto}></RNCamera>
-      <Pressable
-        onPress={() => handleImageCapture()}
-        style={styles.captureButton}></Pressable>
+        whiteBalance={RNCamera.Constants.WhiteBalance.auto}>
+      </RNCamera>
+      <TouchableOpacity
+        onPress={() => _captureImage()}
+        style={styles.captureButton}></TouchableOpacity>
     </View>
   );
 };
